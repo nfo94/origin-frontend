8108632b7bbe375b9c994dde80619cad59af02f0
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-01 12:43:45 -0300

    feat: add button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import PrimaryButton from './styles';
+
+export default function Button() {
+  return <PrimaryButton type="button">Confirm</PrimaryButton>;
+}
---
+++ src/components/Button/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+  background-color: var(--brandColorPrimary);
+  color: var(--neutralWhite);
+  padding: 18px 128px;
+  border-radius: 32px;
+  font-weight: 600;
+  font-size: 1rem;
+  border: none;
+`;
+
+export default PrimaryButton;
