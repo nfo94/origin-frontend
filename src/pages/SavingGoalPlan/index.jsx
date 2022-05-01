3f89b9eeeb04b9e38d72219b3a93369fe22e5bbf
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-01 12:41:06 -0300

    feat: add SavingGoalPlan page

---
+++ src/pages/SavingGoalPlan/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Card from 'components/Card';
+import Container from './styles';
+
+export default function SavingGoalPlan() {
+  return (
+    <Container>
+      <h3>
+        Let&apos;s plan your <strong>saving goal</strong>.
+      </h3>
+      <Card />
+    </Container>
+  );
+}
---
+++ src/pages/SavingGoalPlan/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Container = styled.main`
+  display: flex;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 2.5rem;
+
+  h3 {
+    margin-bottom: 1.5rem;
+    font-weight: 400;
+    color: var(--brandColorPrimary);
+    text-align: center;
+  }
+`;
+
+export default Container;
