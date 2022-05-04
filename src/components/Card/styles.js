2637d1318044020c541f4d79b6287cc3d6088563
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-03 21:58:47 -0300

    style: add CardInputs style to Card

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+export const Container = styled.div`
   background: var(--neutralWhite);
   border: 1px solid var(--blueGray10);
   box-shadow: 0px 16px 32px rgba(30, 42, 50, 0.08);
@@ -34,4 +34,25 @@ const Container = styled.div`
   }
 `;
 
-export default Container;
+export const CardInputs = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  @media (min-width: 320px) and (max-width: 868px) {
+    flex-direction: column;
+  }
+
+  label {
+    color: var(--blueGray900);
+    font-size: 0.9rem;
+  }
+
+  label:not(:last-child) {
+    margin-right: 16px;
+
+    @media (min-width: 320px) and (max-width: 868px) {
+      margin-right: 0;
+      margin-bottom: 16px;
+    }
+  }
+`;
