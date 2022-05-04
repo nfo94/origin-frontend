162a73c78bf248f39a97c43198fa587c693cfed2
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-03 21:59:21 -0300

    style: refactor CardResult styles

--- src/components/CardResult/styles.js
+++ src/components/CardResult/styles.js
@@ -7,31 +7,42 @@ const Result = styled.div`
   border-radius: 4px;
 
   .resultAmount {
-    padding: 20px;
+    padding: 30px 30px 20px 30px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    @media (min-width: 320px) and (max-width: 868px) {
+      padding: 30px 20px 20px 20px;
+    }
 
     .resultAmount--text {
       color: var(--blueGray900);
       font-weight: 400;
       font-size: 1.2rem;
+
+      @media (min-width: 320px) and (max-width: 868px) {
+        font-size: 1.1rem;
+      }
     }
 
     .resultAmount--number {
       font-family: 'Rubik', sans-serif;
       color: var(--brandColorSecondary);
       font-size: 2rem;
+
+      @media (min-width: 320px) and (max-width: 868px) {
+        font-size: 1.4rem;
+      }
     }
   }
 
   .resultSummary {
     background-color: var(--blueGray10);
-    padding: 20px;
+    padding: 24px 32px 24px 32px;
     color: var(--blueGray900);
 
     p {
-      font-size: 0.8rem;
+      font-size: 0.9rem;
     }
   }
 `;
