a6c4510a5e2c5649c88dcaffdfed6c458a29e132
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-03 22:06:42 -0300

    feat: implement business logic with local state in Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import Button from 'components/Button';
+import { format, add, sub, compareAsc, eachMonthOfInterval } from 'date-fns';
+
+import { NEXT_DATE, NEXT_MONTH, NEXT_YEAR, OPERATIONS } from 'utils/constants';
 import CardHeader from 'components/CardHeader';
-import CardInputs from 'components/CardInputs';
+import AmountInput from 'components/AmountInput';
+import MonthYearInput from 'components/MonthYearInput';
 import CardResult from 'components/CardResult';
-import Container from './styles';
+import Button from 'components/Button';
+
+import { Container, CardInputs } from './styles';
 
 export default function Card() {
+  const [amount, setAmount] = useState('');
+  const [month, setMonth] = useState(NEXT_MONTH);
+  const [year, setYear] = useState(NEXT_YEAR);
+  const [date, setDate] = useState(NEXT_DATE);
+  const [monthlyAmount, setMonthlyAmount] = useState('');
+  const [monthlyDeposits, setMonthlyDeposits] = useState('');
+
+  const handleResult = () => {
+    const interval = eachMonthOfInterval({
+      start: Date.parse(NEXT_DATE),
+      end: Date.parse(date),
+    });
+    const parsedMonths = parseFloat(interval.length);
+    const parsedAmount = amount === '' ? 0 : amount.replace(/,/g, '');
+
+    if (parsedAmount === 0 && parsedMonths === 0) {
+      setMonthlyDeposits('');
+      setMonthlyAmount('');
+      return;
+    }
+    const result = (parsedAmount / parsedMonths).toFixed(2);
+    setMonthlyAmount(result);
+    setMonthlyDeposits(parsedMonths.toString());
+  };
+
+  useEffect(() => {
+    handleResult();
+  }, [month, year, amount]);
+
+  const handleReachDate = operation => {
+    if (operation === OPERATIONS.add) {
+      const newDate = add(date, { months: 1 });
+      const newMonth = format(newDate, 'LLLL');
+      const newYear = format(newDate, 'yyyy');
+
+      setDate(newDate);
+      setMonth(newMonth);
+      setYear(newYear);
+    }
+    if (operation === OPERATIONS.subtract) {
+      const newDate = sub(date, { months: 1 });
+      if (compareAsc(NEXT_DATE, newDate) === 1) {
+        setMonth(month);
+        setYear(year);
+      } else {
+        const newMonth = format(newDate, 'LLLL');
+        const newYear = format(newDate, 'yyyy');
+
+        setDate(newDate);
+        setMonth(newMonth);
+        setYear(newYear);
+      }
+    }
+  };
+
   return (
     <Container>
       <div className="item">
@@ -14,15 +74,24 @@ export default function Card() {
       </div>
 
       <div className="item">
-        <CardInputs />
+        <CardInputs>
+          <AmountInput amount={amount} handleTotalAmount={value => setAmount(value)} />
+          <MonthYearInput month={month} year={year} handleReachDate={handleReachDate} />
+        </CardInputs>
       </div>
 
       <div className="item">
-        <CardResult />
+        <CardResult
+          monthlyAmount={monthlyAmount}
+          monthlyDeposits={monthlyDeposits}
+          amount={amount}
+          month={month}
+          year={year}
+        />
       </div>
 
       <div className="item btn">
-        <Button />
+        <Button text="Confirm" />
       </div>
     </Container>
   );
