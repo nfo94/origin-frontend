7f7b7b8f822b745e3910553258f9151f2d9991d0
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-03 22:04:23 -0300

    refactor: make Button component reusable

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import PrimaryButton from './styles';
 
-export default function Button() {
-  return <PrimaryButton type="button">Confirm</PrimaryButton>;
+export default function Button({ text }) {
+  return <PrimaryButton type="button">{text}</PrimaryButton>;
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
