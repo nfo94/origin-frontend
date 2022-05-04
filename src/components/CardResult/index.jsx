0d1b063859cbdacffb58d5c84cfdf69887017cfc
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-03 22:05:09 -0300

    refactor: update CardResult functionality

--- src/components/CardResult/index.jsx
+++ src/components/CardResult/index.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import Result from './styles';
 
-export default function Card() {
+export default function CardResult({ monthlyAmount, monthlyDeposits, amount, month, year }) {
   return (
     <Result>
       <div className="resultAmount">
         <p className="resultAmount--text">Monthly amount</p>
-        <p className="resultAmount--number">$520.83</p>
+        <p className="resultAmount--number">
+          {Intl.NumberFormat('en-IN', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+          }).format(monthlyAmount)}
+        </p>
       </div>
       <div className="resultSummary">
         <p>
-          You&apos;re planning <strong>48 monthly deposits</strong> to reach you&apos;{' '}
-          <strong>$2000.00</strong> goal by October 2020
+          You&apos;re planning <strong>{monthlyDeposits} monthly deposit(s)</strong> to reach your{' '}
+          <strong>${amount}</strong> goal by{' '}
+          <strong>
+            {month} {year}
+          </strong>
+          .
         </p>
       </div>
     </Result>
   );
 }
+
+CardResult.propTypes = {
+  monthlyAmount: PropTypes.string.isRequired,
+  monthlyDeposits: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  month: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+};
